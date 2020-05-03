99eb5d0f0478b4ca72208cb050cf78a3bf8a55a9
Vargas1723 <vargas23.bv@gmail.com>
2020-05-02 20:00:15 -0500

    Store Location
    GoogleMap con localizacion de la tienda

--- app/src/main/java/com/happynacho/bookland/FragmentMaps.java
+++ app/src/main/java/com/happynacho/bookland/FragmentMaps.java
@@ -9,13 +9,67 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentMaps extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class FragmentMaps extends Fragment implements OnMapReadyCallback {
 
+    private static FragmentMaps INSTANCE = null;
+
+    View view;
+
+    GoogleMap map;
+    MapView mapView;
+
+    public FragmentMaps(){
+
+    }
+    public static FragmentMaps getINSTANCE(){
+        if(INSTANCE == null)
+            INSTANCE = new FragmentMaps();
+        return INSTANCE;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.frag_maps, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = view.findViewById(R.id.mapsView);
+        if( mapView != null){
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+    @NonNull
+    private CameraPosition getCameraPositionWithBearing(LatLng latLng) {
+        return new CameraPosition.Builder().target(latLng).zoom(16).build();
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize((getContext()));
+        map = googleMap;
+        LatLng location = new LatLng(20.6754563,-103.3671016);
+        map.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker()).position(location));
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(getCameraPositionWithBearing(location)));
+    }
+
 }
