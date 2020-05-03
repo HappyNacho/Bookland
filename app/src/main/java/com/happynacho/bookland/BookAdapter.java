d2bdad94dfe1cf8cecfa78a17e88eec9d1a8e4e5
Vargas1723 <vargas23.bv@gmail.com>
2020-05-03 16:46:52 -0500

    Alert Dialog
    Alert dialog with description

--- app/src/main/java/com/happynacho/bookland/BookAdapter.java
+++ app/src/main/java/com/happynacho/bookland/BookAdapter.java
@@ -38,15 +38,35 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull BookViewHolder holder, final int position) {
         holder.title.setText("Title: "+book_list.get(position).getTitle());
         holder.author.setText("Author: "+book_list.get(position).getAuthor());
         holder.category.setText("Category: "+book_list.get(position).getCategory());
         holder.editorial.setText("Editorial: "+book_list.get(position).getEditorial());
-        holder.description.setText("Description: "+book_list.get(position).getDescription());
+        //holder.description.setText("Description: "+book_list.get(position).getDescription());
         holder.price.setText("Price: "+book_list.get(position).getPrice());
         Picasso.with(context).load(book_list.get(position).getUrl_image()).placeholder(R.mipmap.ic_launcher_round).into(holder.image);
+        holder.getItemView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(context)
+                        .setTitle("Description")
+                        .setMessage(book_list.get(position).getDescription())
 
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Continue with delete operation
+                            }
+                        })
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
 
     }
 
@@ -60,15 +80,19 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder
     public class BookViewHolder extends RecyclerView.ViewHolder {
         TextView title,author,category,editorial,description,price;
         ImageView image;
+        View view;
         public BookViewHolder (View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.textViewTitle);
             author = itemView.findViewById(R.id.textViewAuthor);
             category = itemView.findViewById(R.id.textViewCategory);
             editorial = itemView.findViewById(R.id.textViewEditorial);
-            description = itemView.findViewById(R.id.textViewDescription);
+            //description = itemView.findViewById(R.id.textViewDescription);
             price = itemView.findViewById(R.id.textViewPrice);
             image = itemView.findViewById(R.id.imageView);
+            view=itemView;
+
         }
+        public View getItemView(){ return view;}
     }
 }
